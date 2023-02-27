fcfc3c07cd24d0b5952dfbe4a147ec1f69d46c5f
Raissa Correia <raissa.correia@pyyne.com>
2023-02-27 14:42:39 -0300

    HTTP response

---
+++ src/main/java/com/challenge/hiringtest/payloads/BankIdDto.java
@@ -0,0 +1,19 @@
+
+public class BankIdDto {
+    private int id;
+
+    public BankIdDto() {
+    }
+
+    public BankIdDto(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
