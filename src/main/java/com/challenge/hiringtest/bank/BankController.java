b51e4839827afb34534e3dc905921d588594771f
Raissa Correia <raissa.correia@pyyne.com>
2023-02-27 14:36:48 -0300

    HTTP response

--- src/main/java/com/challenge/hiringtest/bank/BankController.java
+++ src/main/java/com/challenge/hiringtest/bank/BankController.java
@@ -7,20 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
 
 @RestController
 @RequestMapping("")
 public class BankController {
 
     @GetMapping("/transactions")
-    public void printTransactions(@RequestParam(value = "bankId", defaultValue = "0", required = false) int id) {
+    public ResponseEntity<List<String>> printTransactions(
+            @RequestParam(value = "bankId", defaultValue = "0", required = false) int id
+    )
+    {
         BankTransactions bankTransactions = new BankTransactions(id);
-        bankTransactions.printTransactions().forEach(System.out::println);
+        return ResponseEntity.ok(bankTransactions.printTransactions());
     }
 
     @GetMapping("/balances")
-    public void printBalances(@RequestParam(value = "bankId", defaultValue = "0", required = false) int id) {
+    public ResponseEntity<String> printBalances(
+            @RequestParam(value = "bankId", defaultValue = "0", required = false) int id
+    )
+    {
         BankBalance bankBalance = new BankBalance(id);
-        System.out.println(bankBalance.printBalance());
+        return ResponseEntity.ok(bankBalance.printBalance());
     }
 }
